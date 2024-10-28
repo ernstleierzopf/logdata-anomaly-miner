0778d4c85c015d126e7471b61be46563f5ed4e60
Ernst Leierzopf <56120276+ernstleierzopf@users.noreply.github.com>
2024-10-28 14:33:48 +0100

    create ZmqEventHandler unittests (#1350)
    * add first draft oft ZmqEventHandler unittests.
    
    * implement ZmqEventHandler unittests.

--- source/root/usr/lib/logdata-anomaly-miner/aminer/events/ZmqEventHandler.py
+++ source/root/usr/lib/logdata-anomaly-miner/aminer/events/ZmqEventHandler.py
@@ -30,12 +30,29 @@ class ZmqEventHandler(EventHandlerInterface):
         @param topic the topic used in the Zero Message Queue.
         @param url the internal inter process communication channel.
         """
+        if not isinstance(url, (str, bytes)):
+            msg = "url has to be of the type string or bytes."
+            logging.getLogger(DEBUG_LOG_NAME).error(msg)
+            raise TypeError(msg)
+        if url is not None and len(url) == 0:
+            msg = "url must not be empty."
+            logging.getLogger(DEBUG_LOG_NAME).error(msg)
+            raise ValueError(msg)
+        if not isinstance(topic, str):
+            msg = "topic has to be of the type string."
+            logging.getLogger(DEBUG_LOG_NAME).error(msg)
+            raise TypeError(msg)
+        if topic is not None and len(topic) == 0:
+            msg = "topic must not be empty."
+            logging.getLogger(DEBUG_LOG_NAME).error(msg)
+            raise ValueError(msg)
         self.analysis_context = analysis_context
         self.url = url
         self.topic = topic
-        self.producer = None
-        self.context = None
-        self.zmq_imported = False
+        self.context = zmq.Context()
+        self.producer = self.context.socket(zmq.PUB)
+        self.producer.bind(self.url)
+        time.sleep(1)
         logging.getLogger(DEBUG_LOG_NAME).info("ZmqEventHandler initialized")
 
     def receive_event(self, _event_type, _event_message, _sorted_loglines, event_data, _log_atom, event_source):
@@ -58,19 +75,6 @@ class ZmqEventHandler(EventHandlerInterface):
         component_name = self.analysis_context.get_name_by_component(event_source)
         if component_name in self.analysis_context.suppress_detector_list:
             return True
-        if self.zmq_imported is False:
-            try:
-                self.context = zmq.Context()
-                self.producer = self.context.socket(zmq.PUB)
-                self.producer.bind(self.url)
-                time.sleep(1)
-                logging.getLogger(DEBUG_LOG_NAME).info("Created socket on %s", self.url)
-                self.zmq_imported = True
-            except ImportError:
-                msg = 'ZeroMQ module not found.'
-                logging.getLogger(DEBUG_LOG_NAME).error(msg)
-                print('ERROR: ' + msg, file=sys.stderr)
-                return False
         if not isinstance(event_data, str) and not isinstance(event_data, bytes):
             msg = 'ZmqEventHandler received non-string event data. Use the JsonConverterHandler to serialize it first.'
             logging.getLogger(DEBUG_LOG_NAME).warning(msg)
@@ -93,6 +97,5 @@ class ZmqEventHandler(EventHandlerInterface):
             print("Error: " + msg, file=sys.stderr)
             self.producer.close()
             self.producer = None
-            self.zmq_imported = False
             return False
         return True
