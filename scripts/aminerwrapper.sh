6f24b41d2f769935e765ff093c512cc87a49f759
Ernst Leierzopf <e.leierzopf@gmail.com>
2025-02-20 17:49:05 +0100

    exclude venv from tests.

--- scripts/aminerwrapper.sh
+++ scripts/aminerwrapper.sh
@@ -4,13 +4,19 @@ AMINERDIR=/usr/lib/logdata-anomaly-miner
 
 case "$1" in
 	aminer)
+	    $AMINERDIR/.venv/bin/activate
 		$AMINERDIR/aminer.py ${*:2}
+		deactivate
 		;;
 	aminerremotecontrol)
+	    $AMINERDIR/.venv/bin/activate
 		$AMINERDIR/aminerremotecontrol.py ${*:2}
+		deactivate
 		;;
-        aminer-persistence)
+    aminer-persistence)
+	    $AMINERDIR/.venv/bin/activate
 		$AMINERDIR/aminer-persistence.py ${*:2}
+		deactivate
 		;;
 	supervisor)
 		/usr/bin/supervisord
